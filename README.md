87ae9b62af3e607103648b6168e7636f0adb8a00
Rubén Fernández Gordillo <7360184+rubfergor@users.noreply.github.com>
2021-04-01 13:00:17 +0200

    feat: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # modulo-calculator
+
+This is a simple utility to calculate the modulo of packages.
+
+# Install
+
+Clone the repository and then execute:
+
+`yarn install`
+
+After that, you should have a `dist/` directory with the Javascript files. You can use node to run them but if you prefer to make an executable, run:
+
+`yarn package`
+
+This will create executables of the files for mac, linux and windows.
+
+# Use
+
+1. Provide a bitcoin transaction hash
+2. Specify the divisor
+3. ???
+4. Profit!! (well, you may not be the one who profits, but anyways, profit!)
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import BigNumber from "bignumber.js";
+import { prompt } from "inquirer";
+import chalk from "chalk";
+
+const run = async (): Promise<void> => {
+  const { hash, divisor } = await prompt([
+    {
+      name: "hash",
+      type: "input",
+      message: "Hash",
+    },
+    {
+      name: "divisor",
+      type: "number",
+      message: "Divisor",
+    },
+  ]);
+
+  console.log(
+    `The modulo of the operation is ${chalk.green(
+      new BigNumber(hash, 16).modulo(divisor).toString()
+    )}`
+  );
+  return;
+};
+
+run().catch((error) => console.error(error));
